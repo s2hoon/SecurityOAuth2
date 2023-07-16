be03ed28fd1cedc72fec5a1c91b31caf7153709e
suhoon <jobak4200nim@nate.com>
2023-07-17 01:09:46 +0900

    Members 조회 추가

--- src/main/java/com/example/SecurityOAuth2/Repository/MemberRepository.java
+++ src/main/java/com/example/SecurityOAuth2/Repository/MemberRepository.java
@@ -3,8 +3,11 @@ package com.example.SecurityOAuth2.Repository;
 import com.example.SecurityOAuth2.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    List<Member> findAll();
 }
--- src/main/java/com/example/SecurityOAuth2/controller/AuthController.java
+++ src/main/java/com/example/SecurityOAuth2/controller/AuthController.java
@@ -2,32 +2,46 @@ package com.example.SecurityOAuth2.controller;
 
 import com.example.SecurityOAuth2.Dto.Request.KaKaoLoginParams;
 import com.example.SecurityOAuth2.Dto.Request.NaverLoginParams;
-import com.example.SecurityOAuth2.service.OAuthLoginService;
+import com.example.SecurityOAuth2.Repository.MemberRepository;
 import com.example.SecurityOAuth2.config.AuthTokens;
+import com.example.SecurityOAuth2.entity.Member;
+import com.example.SecurityOAuth2.service.OAuthLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class AuthController {
     private final OAuthLoginService oAuthLoginService;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/loginForm")
     public String logininForm() {
         return "loginForm";
     }
 
-    @PostMapping("/auth/kakao/callback")
+    @GetMapping("/auth/kakao/callback")
     public ResponseEntity<AuthTokens> loginKakao(String code) {
         KaKaoLoginParams params = new KaKaoLoginParams(code);
         return ResponseEntity.ok(oAuthLoginService.login(params));
     }
-    @PostMapping("/auth/naver/callback")
+
+    @GetMapping("/auth/naver/callback")
     public ResponseEntity<AuthTokens> loginNaver(String code) {
-        NaverLoginParams params = new NaverLoginParams(code,"test");
+        NaverLoginParams params = new NaverLoginParams(code, "test");
         return ResponseEntity.ok(oAuthLoginService.login(params));
+    }
+
+
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> getMembers() {
+        List<Member> allMembers = memberRepository.findAll();
+        return ResponseEntity.ok(allMembers);
     }
 }
